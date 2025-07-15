de5f04095b8584ac8b33d95b08ee7636a3f6a037
acuavista <stanp193@gmail.com>
2025-07-15 17:26:34 -0400

    Update Footer.tsx   added // at the end of
    simulate news letter line

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
     setIsSubscribing(true);
 
     try {
-      // Simulate newsletter subscription
+      // Simulate newsletter subscription//
       await new Promise(resolve => setTimeout(resolve, 1000));
       
       toast({
